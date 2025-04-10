e8f1b4508ec4ccdb22ea5a2d5f1ab73b6ca7f13b
Dedrick Buckley <DedrickBuckley5@gmail.com>
2025-04-10 11:40:11 -0500

    completed shoppinglist

---
+++ shopping-list/src/main/java/com/pluralsight/shoppingList.java
@@ -0,0 +1,32 @@
+
+package com.pluralsight;
+
+
+public class shoppingList {
+
+
+    public static void main(String[] args) {
+
+        // 4. Print the shopping list
+        System.out.println("--- My Shopping List ---");
+        System.out.println("- Milk (1 Gallon)");
+        System.out.println("- Eggs (Dozen)");
+        System.out.println("- Bread (Whole Wheat)");
+        System.out.println("- Butter");
+        System.out.println("- Apples (5)");
+        System.out.println("- Bananas (Bunch)");
+        System.out.println("- Chicken Breasts (1 lb)");
+        System.out.println("- Rice (Basmati, 1kg)");
+        System.out.println("- Pasta (Spaghetti)");
+        System.out.println("- Tomato Sauce");
+        System.out.println("- Coffee Beans");
+        System.out.println("- Orange Juice");
+        System.out.println("------------------------");
+
+    }
+}
+
+
+
+
+
