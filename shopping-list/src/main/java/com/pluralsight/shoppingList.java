c012a44787c4f268c2ef6c7c9202c61f26b55db5
Dedrick Buckley <DedrickBuckley5@gmail.com>
2025-04-10 12:02:44 -0500

    updated shopping list

--- shopping-list/src/main/java/com/pluralsight/shoppingList.java
+++ shopping-list/src/main/java/com/pluralsight/shoppingList.java
@@ -7,17 +7,17 @@ public class shoppingList {
 
     public static void main(String[] args) {
 
-        // 4. Print the shopping list
+
         System.out.println("--- My Shopping List ---");
-        System.out.println("- Milk (1 Gallon)");
-        System.out.println("- Eggs (Dozen)");
-        System.out.println("- Bread (Whole Wheat)");
+        System.out.println("- Milk ");
+        System.out.println("- Eggs");
+        System.out.println("- Bread");
         System.out.println("- Butter");
-        System.out.println("- Apples (5)");
-        System.out.println("- Bananas (Bunch)");
-        System.out.println("- Chicken Breasts (1 lb)");
-        System.out.println("- Rice (Basmati, 1kg)");
-        System.out.println("- Pasta (Spaghetti)");
+        System.out.println("- Apples");
+        System.out.println("- Bananas");
+        System.out.println("- Chicken Breasts ");
+        System.out.println("- Rice ");
+        System.out.println("- Pasta ");
         System.out.println("- Tomato Sauce");
         System.out.println("- Coffee Beans");
         System.out.println("- Orange Juice");
